0f8c41a36d3cd835697641536501c93cefcd99b7
Oleg Kovalev <kovalev.o.d@list.ru>
2024-01-28 12:51:44 +0300

    Initial Commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Олег Ковалев on 28.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
